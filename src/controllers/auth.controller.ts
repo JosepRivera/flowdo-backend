75b43657af2f4341bef776d6e03fd1fb615e90d4
JosepRivera <josepdanton1518@gmail.com>
2026-01-05 17:24:43 -0500

    feat: add refresh and logout routes to authentication controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { authService } from "../services/auth.service.js";
+import { access } from "node:fs";
 
 export const authController = {
   async register(req: Request, res: Response) {
@@ -25,6 +26,40 @@ export const authController = {
       res.json(result);
     } catch {
       res.status(401).json({ message: "Invalid credentials" });
+    }
+  },
+
+  async refresh(req: Request, res: Response) {
+    try {
+      const refreshToken = req.cookies?.refresh_token;
+
+      if (!refreshToken) {
+        return res.status(401).json({ message: "No refresh token" });
+      }
+
+      const result = await authService.refresh(refreshToken);
+
+      res.cookie("refresh_token", result.refreshToken, {
+        httpOnly: true,
+      });
+
+      console.log("Cookies received:", req.cookies);
+
+      return res.json({ accessToken: result.accessToken });
+    } catch (error) {
+      return res.status(401).json({ message: "Refresh failed" });
+    }
+  },
+  
+  async logout(req: Request, res: Response) {
+    try {
+      const refreshToken = req.cookies?.refresh_token;
+      await authService.logout(refreshToken);
+
+      res.clearCookie("refresh_token");
+      return res.sendStatus(204);
+    } catch {
+      return res.status(500).json({ message: "Logout failed" });
     }
   },
 };
